3ae1e4e15bd74a502f588fd59598d3ab99e6947e
brndn <brendan.meade@enxus.co.uk>
2017-04-18 21:48:57 +0200

    final site works..

---
+++ views/view1.js
@@ -0,0 +1,262 @@
+// standard global variables
+var camera, tick = 0,
+    useGl,
+    scene, renderer, clock = new THREE.Clock(true),
+    controls, container, stats,
+    options, spawnerOptions, particleSystem;
+
+function initPage1(){
+
+    init();
+    animate()
+}
+
+// FUNCTIONS
+function init() {
+
+
+    container = document.getElementById( 'home-gl' );
+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    // useGl = false;
+    // return;
+    if(isFirefox || (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()))){
+
+        $(container).addClass('no-gl');
+        useGl = false;
+        return;
+
+    }else {
+
+        $(container).addClass('with-gl');
+        useGl = true;
+        
+    }
+
+
+
+
+    camera = new THREE.PerspectiveCamera(36, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 140;
+
+    scene = new THREE.Scene();
+
+
+    // The GPU Particle system extends THREE.Object3D, and so you can use it
+    // as you would any other scene graph component.	Particle positions will be
+    // relative to the position of the particle system, but you will probably only need one
+    // system for your whole scene
+
+
+    particleSystem = new THREE.GPUParticleSystem({
+       /// maxParticles: 250000
+        maxParticles: 250000
+    });
+
+
+
+    // options passed during each spawned
+    options = {
+        position: new THREE.Vector3(0.1,4,12),
+        positionRandomness: 2.35,
+        velocity: new THREE.Vector3(22,12,6),
+        velocityRandomness: 2.8,
+        color: 0xFFAA62,
+        colorRandomness: 0.1,
+        turbulence: 0.8,
+        lifetime: 25,
+        size: 3.5,
+        sizeRandomness: 12.7
+    };
+
+    spawnerOptions = {
+        spawnRate: 30000,
+        horizontalSpeed: 7.8,
+        verticalSpeed: 2.18,
+        timeScale: 0.6
+    }
+
+    //        gui.add(options, "velocityRandomness", 0, 3);
+    //        gui.add(options, "positionRandomness", 0, 3);
+    //        gui.add(options, "size", 1, 20);
+    //        gui.add(options, "sizeRandomness", 0, 25);
+    //        gui.add(options, "colorRandomness", 0, 1);
+    //        gui.add(options, "lifetime", .1, 10);
+    //        gui.add(options, "turbulence", 0, 1);
+    //
+    //        gui.add(spawnerOptions, "spawnRate", 10, 30000);
+    //        gui.add(spawnerOptions, "timeScale", -1, 1);
+
+
+    renderer = new THREE.WebGLRenderer();
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    //renderer.setClearColor( 0xffffff );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+    container.appendChild(renderer.domElement);
+
+
+
+
+
+
+
+    // cube geometry
+    var geometry = new THREE.BoxGeometry();
+
+    // texture
+    // var texture = new THREE.Texture( generateTexture( ) ); // texture background is transparent
+    // texture.needsUpdate = true; // important
+    //var texture = THREE.ImageUtils.loadTexture("snowflake7_alpha.png");
+
+
+    scene.add(particleSystem);
+
+    function generateTexture() {
+
+        // create canvas
+        var canvas = renderer.domElement;
+        canvas.width = renderer.domElement.width;
+        canvas.height = renderer.domElement.height;
+
+        // get context
+        var context = canvas.getContext( '3d' );
+
+
+
+
+        var mask = document.createElement('img');
+        mask.src = 'resources/images/3dFace2.png';
+
+        mask.onload = function() {
+            context.width  = renderer.domElement.width;
+            context.height = renderer.domElement.height;
+
+            context.globalCompositeOperation='destination-in';
+
+            context.drawImage(mask, 10, 10, width, height);
+
+
+
+            //img.src = imagecanvas.toDataURL();
+        }
+
+
+
+
+
+        return canvas;
+
+    }
+
+
+    // setup controls
+    /**
+     *
+     * @type {THREE.TrackballControls}
+     */
+    controls = new THREE.TrackballControls(camera, renderer.domElement);
+    controls.rotateSpeed = 5.0;
+    controls.zoomSpeed = 2.2;
+    controls.panSpeed = 1;
+    controls.dynamicDampingFactor = 0.3;
+
+
+    // stats = new Stats();
+    // container.appendChild( stats.dom );
+
+    
+   /// window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+
+function animate() {
+
+    if(!useGl){
+        return;
+
+    }
+
+    requestAnimationFrame(animate);
+
+
+
+
+    //   controls.update();
+
+
+
+    var delta = clock.getDelta() * spawnerOptions.timeScale;
+    tick += delta;
+
+    if (tick < 0) tick = 0;
+
+    if (delta > 0) {
+        options.position.x = Math.sin(tick * spawnerOptions.horizontalSpeed) * 35;
+        options.position.y = Math.sin(tick * spawnerOptions.verticalSpeed) * 10;
+        // options.position.y = 0
+        options.position.z = Math.sin(tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed) * 5;
+
+        for (var x = 0; x < spawnerOptions.spawnRate * delta; x++) {
+            // Yep, that's really it.	Spawning particles is super cheap, and once you spawn them, the rest of
+            // their lifecycle is handled entirely on the GPU, driven by a time uniform updated below
+            particleSystem.spawnParticle(options);
+        }
+    }
+
+    particleSystem.update(tick);
+
+    //stats.update();
+
+    renderer.render(scene, camera);
+
+
+}
+function reInitPage1(){
+
+    if(!useGl){
+        return;
+
+    }
+
+    scene.add(particleSystem);
+    animate();
+}
+
+
+function removeParticles(){
+
+    if(!useGl){
+        return;
+
+    }
+    scene.remove(particleSystem);
+
+    // animate();
+    cancelAnimationFrame(animate)
+
+    // scene.dispose();
+    // renderer.dispose();
+
+    // container.removeChild(renderer.domElement);
+
+    // particleSystem = null;
+    //        removeEntity(particleSystem);
+    //        scene.dispose();
+
+}
+
+
+  
---
+++ views/view2.js
@@ -0,0 +1,276 @@
+
+var portfilio = [
+    {imageThumb: '1_Screenshot.jpg', movieUrl: '213228273',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Game Design. End of year Game project. Completed in 3 months. 1st game project. Concept creation, Environment Modelling, lighting, Character, Menu, level design and gameplay. Pre-Alpha Game.<br/><br/><b>Software:</b> Maya, Photoshop, Unity, After Effects, Fuse</p><button>click to watch</button>")
+    },
+    {
+imageThumb: '2_Screenshot.jpg',
+        movieUrl: '213228870',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Small Environment Modelling 1st time. Concept design, Title sequence, Logo, Menu, Rendering.<br/><br/><b>Software:</b> Maya, Photoshop, After Effects</p><button>click to watch</button>")
+    },
+
+    {imageThumb: '3_Screenshot.jpg', movieUrl: '213229561',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Organic Character Modelling, Texturing, Lighting, Rendering.<br/><br/><b>Software:</b> Maya, Photoshop, Mental Ray.</p><button>click to watch</button>")
+    },
+    {
+        imageThumb: '4_Screenshot.jpg',
+        movieUrl: '213229662',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> 3000 Poly Character design, Modelling, Texturing, Lighting, Rendering.<br/><br/><b>Software:</b> 3DS Max, Photoshop, Mental Ray.</p><button>click to watch</button>")
+    },
+    {imageThumb: '5_Screenshot.jpg', movieUrl: '213229918',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> HeadPhone Modelling, Lighting, Rendering.<br/><br/><b>Software:</b> Maya, Mental Ray.</p><button>click to watch</button>")
+    },
+
+    {
+        imageThumb: '5_1Screenshot.jpg',
+        imageLarge: '5_1Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Sculpting, Polypainting. First sculpt in Zbrush.<br/><br/><b>Software:</b> Zbrush.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '5_2Screenshot.jpg',
+        imageLarge: '5_2Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Character Design, Sculpting, Polypainting.<br/><br/><b>Software:</b> Zbrush.</p><button>click to view</button>")
+    },
+
+    {
+        imageThumb: '6_Screenshot.jpg',
+        imageLarge: '6_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Concept Game inspired by Temple run for a Leading DJ.<br/><br/><b>Software:</b> Maya, Fuse, Unity, Photoshop.</p><button>click to view</button>")
+    },
+    
+    {imageThumb: '7_Screenshot.jpg', imageLarge: '7_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Organic Character Modelling.<br/><br/><b>Software:</b> Maya.</p><button>click to view</button>")
+
+    },
+
+    {
+        imageThumb: '8_Screenshot.jpg',
+        imageLarge: '8_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Low Poly Tree.<br/><br/><b>Software:</b> 3DS Max, Photoshop.</p><button>click to view</button>")
+
+    },
+    {
+        imageThumb: '9_Screenshot.jpg', imageLarge: '9_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Full body Rigging, incl Blendshapes for facial expressions.<br/><br/><b>Software:</b> Maya.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '10_Screenshot.jpg',
+        imageLarge: '10_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Low Poly Clothing & Hair Design.<br/><br/><b>Software:</b> 3DS Max.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '11_Screenshot.jpg', imageLarge: '11_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Logo Design.<br/><br/><b>Software:</b> Photoshop, Adobe Illustrator.</p><button>click to view</button>")
+    },
+
+    {
+        imageThumb: '12_Screenshot.jpg',
+        imageLarge: '12_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Character design Biography.<br/><br/><b>Software:</b> Photoshop.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '15_Screenshot.jpg', imageLarge: '15_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Animal Character Design.<br/><br/><b>Software:</b> Photoshop.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '16_Screenshot.jpg',
+        imageLarge: '16_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Production design end of year project.<br/><br/><b>Software:</b> Photoshop.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '17_Screenshot.jpg', imageLarge: '17_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Environment Design.<br/><br/><b>Software:</b> Photoshop.</p><button>click to watch</button>")
+    },
+    {
+        imageThumb: '18_Screenshot.jpg',
+        imageLarge: '18_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Oil Painting on Canvas.<br/><br/><b>Medium:</b> Oil & Canvas.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '19_Screenshot.jpg', imageLarge: '19_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Oil Painting on Canvas.<br/><br/><b>Medium:</b> Oil & Canvas.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '20_Screenshot.jpg',
+        imageLarge: '20_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Oil Painting on Canvas.<br/><br/><b>Medium:</b> Oil & Canvas.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '21_Screenshot.jpg', imageLarge: '21_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Portrait Drawings.<br/><br/><b>Medium:</b> Pencil & Paper.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '22_Screenshot.jpg',
+        imageLarge: '22_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Female Body.<br/><br/><b>Medium:</b> Pencil & Paper.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '23_Screenshot.jpg', imageLarge: '23_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Male Body.<br/><br/><b>Medium:</b> Pencil & Paper.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '24_Screenshot.jpg',
+        imageLarge: '24_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> Curriculum Vitae.</p><button>click to view</button>")
+    },
+    {
+        imageThumb: '25_Screenshot.jpg', downloadFile: '25_Fullsize.jpg',
+        text: $("<h3>MORE INFO</h3><p><b>Description</b> About me.</p><button>click to view</button>")
+    },
+
+
+]
+
+
+
+
+function createImageDom(e) {
+    return {
+        href: 'resources/images/portfolio-images/' + e.imageLarge,
+        html: $("<img>", {
+            src: "resources/images/portfolio-thumbnails/" + e.imageThumb, alt: ""
+        })
+    }
+
+}
+
+function createVideoDom(e) {
+    return {
+        class: 'popup-vimeo',
+        href: 'http://player.vimeo.com/video/' + e.movieUrl,
+        html:$("<img>", {
+            src: "resources/images/portfolio-thumbnails/" + e.imageThumb, alt: ""
+        })
+    }
+
+}
+
+portfilio.forEach(function (e) {
+
+    $('#portfolio gallery').append($('<div>', {
+        class: 'gallery-inner',
+        html: $("<a>", e.movieUrl ? createVideoDom(e) : createImageDom(e))
+        .append(e.text)
+    }));
+
+})
+
+
+
+
+var elementLength, innerElWidth, currentPosition = 0, increment = 3;
+
+function navigateGallery(pos){
+    var newPos;
+    if(pos === -1 && -(currentPosition + (pos*increment)) > elementLength){
+        newPos = 0;
+    }else if(pos === 1 && (currentPosition === 0)){
+        newPos = -elementLength;
+    }else {
+
+        newPos =  currentPosition + (pos*increment)
+    }
+    currentPosition = newPos;
+    $('#portfolio gallery').animate({left: (innerElWidth)*currentPosition})
+}
+
+
+
+
+$(document).on('ready', function () {
+
+    var is_chrome = /chrome/.test( navigator.userAgent.toLowerCase() );
+    if(is_chrome){
+        $("#portfolio .gallery-shell").addClass('gallery-shell-chrome');
+    }
+
+    var generalElement =  $("#portfolio gallery .gallery-inner:first-child")[0];
+    innerElWidth =  generalElement.offsetWidth;
+    elementLength = portfilio.length-1;
+
+     var leftNavigation = $("#portfolio div.left-nav")
+     var rightNavigation = $("#portfolio div.right-nav")
+
+    leftNavigation.click(function(){
+        navigateGallery(1);
+    });
+
+    rightNavigation.click(function(){
+        navigateGallery(-1);
+    })
+
+
+
+
+        $('#portfolio gallery').magnificPopup({
+
+            delegate: 'a',
+            type: 'image',
+            closeOnContentClick: true,
+            closeBtnInside: false,
+            fixedContentPos: false,
+            mainClass: 'mfp-with-zoom mfp-img-mobile',
+            image: {
+                verticalFit: true,
+                titleSrc: function(item) {
+                    return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+                }
+            },
+            gallery: {
+                enabled: true
+            },
+            zoom: {
+                enabled: false,
+            },
+            callbacks: {
+                open: function () {
+                    console.log('Popup open has been initiated');
+                 
+                }, close: function () {
+                    $("#portfolio div.gallery-shell").addClass("hide-shell");
+                    setTimeout(function(){
+
+                    }, 4000)
+
+                },
+                beforeClose: function() {
+                    // Callback available since v0.9.0
+                    console.log('Popup close has been initiated');
+                },
+                // close: function() {
+                //     console.log('Popup removal initiated (after removalDelay timer finished)');
+                // },
+                afterClose: function() {
+                    $("#portfolio div.gallery-shell").removeClass("hide-shell");
+                    console.log('Popup is completely closed');
+                },
+            }
+
+        });
+
+    $('#portfolio gallery .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: false,
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: false,
+        },
+        callbacks: {
+            open: function () {
+                // Will fire when this exact popup is opened
+                // this - is Magnific Popup object
+            }, close: function () {
+                // Will fire when popup is closed
+            }
+        }
+    });
+
+});
